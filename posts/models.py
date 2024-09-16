52b3424fe28eb967de4ae3086ee5a02658a03a9d
user <umar.azlun@gmail.com>
2024-09-16 23:43:59 +0600

    HW4

--- posts/models.py
+++ posts/models.py
@@ -1,14 +1,5 @@
 from django.db import models
 
-"""
-Post.object.all() - достает все объекты таблицы без исключений
-Post.object.filter(title="Text") - достает только нужные объекты из таблицы
-Post.object.get(title="Text) - достает только 1 нужный объект
-Post.object.create(title="Text", content="Text content", rate=0) - создает объект
-"""
-
-
-
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
@@ -33,7 +24,7 @@ class Post(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, null=True, blank=True, related_name='posts'
     )
-    tag = models.ManyToManyField(Tag, null=True, blank=True)
+    tag = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f"{self.title}"
@@ -42,3 +33,6 @@ class Post(models.Model):
 class Comment(models.Model):
     text = models.CharField(max_length=400)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return f"Comment on {self.post.title}"
